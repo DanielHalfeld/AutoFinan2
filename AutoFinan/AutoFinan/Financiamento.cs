36a5e7fe5e0fbcbe1b45a1a6499b14ed593c61b6
DanielHalfeld <104243301+DanielHalfeld@users.noreply.github.com>
2023-08-29 18:21:23 -0300

    Add files via upload
    Baseado nessa fórmula:
    https://www3.bcb.gov.br/CALCIDADAO/publico/exibirMetodologiaFinanciamentoPrestacoesFixas.do?method=exibirMetodologiaFinanciamentoPrestacoesFixas

---
+++ AutoFinan/AutoFinan/Financiamento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Finan
+{
+    public class Financiamento
+    {
+
+        double valorParcela;
+
+        public double Calc(double valorVeiculo, double quantidadeParcelas, double taxaMensal)
+        {
+            taxaMensal = taxaMensal / 100;
+            if (taxaMensal == 0)
+            {
+                valorParcela = (valorVeiculo/quantidadeParcelas) + 100; // Exemplo: taxa administrativa de 100
+            }
+            else
+            {
+                double prestacao = valorVeiculo / quantidadeParcelas;
+                valorParcela = (1 - Math.Pow(1 + taxaMensal, -quantidadeParcelas)) / taxaMensal * prestacao;
+            }
+
+            return valorParcela;
+        }
+    }
+}
---
+++ AutoFinan/AutoFinan/Program.cs
@@ -0,0 +1,29 @@
+﻿using Finan;
+using System;
+
+Console.WriteLine("Informe o valor inicial do veículo: ");
+var valorVeiculoStr = Console.ReadLine();
+            
+
+Console.WriteLine("Informe a quantidade de parcelas: ");
+var quantidadeParcelasStr = Console.ReadLine();
+
+Console.WriteLine("Informe a taxa mensal: (em %) ");
+var taxaMensalStr = Console.ReadLine();
+
+double resultado;
+            
+if (double.TryParse(valorVeiculoStr, out double valorVeiculo) && int.TryParse(quantidadeParcelasStr, out int quantidadeParcelas) && double.TryParse(taxaMensalStr, out double taxaMensal))
+{
+    Financiamento finan;
+    finan = new Financiamento();
+    resultado = finan.Calc(valorVeiculo, quantidadeParcelas, taxaMensal);
+    double valorTotal = 0;
+    Console.WriteLine($"O valor de cada parcela é: {resultado:C} o valor total do veículo é: {valorTotal:C}");
+
+}
+else
+{
+    Console.WriteLine($"Os valores são inválidos.");
+}
+        
